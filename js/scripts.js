39013a6d938353d9383614d0adb448e34375ee75
jennifermccarthy & Sean Niesen <student@epicodus.com>
2014-07-18 12:29:47 -0700

    working on jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
+var Category = {
+  initialize: function(name) {
+    this.name = name;
+    this.purchases = [];
+  },
+  totalCost: function(place) {
+    var total = 0;
+    for (var i = 0; i <= this.purchases.length; i++) {
+    total = total + this.purchase.totalCost(i);
+      console.log(total);
+    }
+    return total;
+  }
+};
+
 var Purchase = {
+  item: "",
   price: 0,
   quantity: 0,
   totalCost: function() {
     return this.quantity * this.price;
+  },
+  initialize: function(name) {
+    this.name = name
   }
 };
+
+
+
+
+
+
+
 
 $(document).ready(function() {
   $("form#expense-tracker").submit(function(event) {
@@ -13,15 +39,17 @@ $(document).ready(function() {
 
     $(".cat-list").append("<li><span class='ind-cat'>" + categoryItem + "</span></li>");
 
+    $(".ind-cat").last().click(function(event) {
+      $("#purchases").show();
+      $("#cat-name").text(categoryItem);
+      event.preventDefault();
+    });
 
     var newPurchase = Object.create(Purchase);
 
     newPurchase.item = $("input#item").val();
     newPurchase.quantity = parseInt($("input#quantity").val());
     newPurchase.price = parseInt($("input#price").val());
-
-    console.log(newPurchase.totalCost());
-
 
     $("tbody#purchases").append("<tr>" +
                         "<td>" + newPurchase.item + "</td>" +
--- js/specs.js
+++ js/specs.js
@@ -4,8 +4,43 @@ describe ("Purchase", function() {
       var pizza = Object.create(Purchase);
       pizza.quantity = 1;
       pizza.price = 10;
-      pizza.totalCost;
       pizza.totalCost().should.equal(10);
+    });
+  });
+  describe("initialize", function() {
+    it("will give give a purchase a name", function() {
+      var pizza = Object.create(Purchase);
+      pizza.initialize("pizza");
+      pizza.name.should.equal("pizza");
+    });
+  });
+});
+
+describe ("Category", function() {
+  describe("initialize", function () {
+    it("will give a category a name and display an array of the purched items in that category", function() {
+      var testInitialize = Object.create(Category);
+      testInitialize.initialize();
+      testInitialize.purchases.should.eql([]);
+    });
+  });
+  describe("totalCost", function() {
+    it("will loop through each array element and give a grand total of purchases for that category", function() {
+      var testTotalCost = Object.create(Category);
+      //create a purhcase object and add it to that category
+      var pizza = Object.create(Purchase);
+      pizza.initialize("pizza");
+      pizza.quantity = 2;
+      pizza.price = 10;
+
+      var sandwich = Object.create(Purchase);
+      sandwich.initialize("sandwich");
+      sandwich.quantity = 2;
+      sandwich.price = 5;
+
+      var school = Object.create(Category);
+      school.initialize("school");
+      school.totalCost().should.equal(30);
     });
   });
 });
